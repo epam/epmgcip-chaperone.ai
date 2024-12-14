ce8d858a083aa351a5af382615a256eaafbc71e9
Dmitry Lukyanov <dzima.lukyanau@gmail.com>
2024-12-14 22:23:28 +0400

    Add translation function. (#2)

---
+++ functions/test_llm.py
@@ -0,0 +1,32 @@
+import json
+from unittest import TestCase
+
+from azure.functions import HttpRequest
+
+from functions.function_app import translate_http_trigger
+
+class FunctionUnitTests(TestCase):
+    def test_translate_text(self):
+        # send an empty body, the function only requires parameters from the query string
+        request = HttpRequest(
+            method="GET",
+            url="http://localhost:7071/api/translate_http_trigger",
+            body="",
+            params={"text": "Hello World!", "language": "Belarussian" },
+        )
+        # call the Azure Function
+        response = translate_http_trigger(request)
+        # load the response body as JSON
+        j = json.loads(response.get_body())
+
+        # the function was executed without errors
+        self.assertEqual(200, response.status_code)
+        self.assertEqual("application/json", response.mimetype)
+
+        for keyword in ["Прывітанне", "Свет"]:
+            print("\nChecking: " + keyword + "..")
+            # not 100% reliable yet, since the expected words might be slighly changed
+            self.assertIn(
+                keyword.lower(),
+                j["LLM Response"].lower()
+            )
--- setup.py
+++
@@ -1,5 +0,0 @@
-# required to use main module
-# TODO: clarify setuptools
-from setuptools import setup, find_packages
-
-setup(name="main", version="1.0", packages=find_packages())
